3e259d687695407c947d3a309dd39b2e14a74234
Rison <2462301753@qq.com>
2021-09-29 18:48:42 +0800

    [feat] 随机抽取车辆,得到抽取车辆信息和行车轨迹

--- src/main/scala/com/rison/traffic/util/DateUtils.scala
+++ src/main/scala/com/rison/traffic/util/DateUtils.scala
@@ -3,11 +3,13 @@ package com.rison.traffic.util
 import java.text.SimpleDateFormat
 import java.util.Date
 
+import cn.hutool.core.date.DateUtil
+
 /**
  * @author : Rison 2021/9/28 上午11:39
  *         时间格式工具
  */
-object DateUtils {
+object DateUtils extends DateUtil{
   /**
    * 时间字符串转Long
    *
@@ -19,5 +21,17 @@ object DateUtils {
     val date: Date = format.parse(time)
     date.getTime
   }
+
+  /**
+   * 时间字符串转Date
+   * @param time
+   * @return
+   */
+  def getTimeStringToDate(time: String): Date = {
+    val format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
+    val date: Date = format.parse(time)
+    date
+  }
+
 
 }
